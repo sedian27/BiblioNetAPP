f2555a6611e746e7b1590794bc316c21bdf0f9e7
sedian <diestrasergio@gmail.com>
2022-03-17 08:32:57 -0500

    Agregar archivos de proyecto.

---
+++ BiblioNetAPP/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BiblioNetAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioNetAPP.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookModel Book) 
+        {
+            if (!ModelState.IsValid) 
+            { 
+                return View(Book);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ BiblioNetAPP/Models/BookModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage="{0} is required")]
+        public string BookName { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
